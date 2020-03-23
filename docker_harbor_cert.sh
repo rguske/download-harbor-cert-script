d3dbf88a336c41ede057eee66adfc6aea419a010
Robert Guske <31652019+rguske@users.noreply.github.com>
2020-03-23 21:04:39 +0100

    Added docker_harbor_cert.sh to the repository

---
+++ docker_harbor_cert.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Ask for the Harbor FQDN and port
+echo "Enter the FQDN and port (harbor.domain.com:443) of your Harbor registry:"
+
+read REGISTRY
+
+# Create folder for custom certificate as described in Docker docs https://docs.docker.com/engine/security/certificates/
+mkdir -p /etc/docker/certs.d/$REGISTRY
+
+# Download Registry Root Certificate
+wget -O etc/docker/certs.d/$REGISTRY/ca.crt https://$REGISTRY/api/systeminfo/getcert --no-check-certificate
+
+# Restart Docker service
+systemctl restart docker
